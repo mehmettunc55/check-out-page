30c6f700485eaa88a94b8ada2f6d113719d2f5b5
mehmettunc55 <mtunc551980@gmail.com>
2024-02-27 15:12:04 +0100

    all buttons customized

--- checkout.js
+++ checkout.js
@@ -1,43 +1,77 @@
-
-const deleteAllBtn =document.querySelector('.delete-div .fa-trash-can')
-const products = document.querySelector('article.products')
+const deleteAllBtn = document.querySelector(".delete-div .fa-trash-can");
+const products = document.querySelector("article.products");
 // const minus = document.querySelector('.fa-minus')
 
+deleteAllBtn.addEventListener("click", () => {
+  products.textContent = "No product";
+  products.classList.add("no-product");
+  document.querySelector(".delete-div").remove();
+  // document.querySelector('.delete-div').style.display = 'none'
+  calculateTotalPrice()
+});
 
-deleteAllBtn.addEventListener('click', () => {
-    
-    products.textContent = 'No product'
-    products.classList.add('no-product')
-    document.querySelector('.delete-div').remove() 
-    // document.querySelector('.delete-div').style.display = 'none'
-} )
-
-products.addEventListener('click', (e) => {
-    
-    if (e.target.classList.contains('fa-plus')){
-e.target.previousElementSibling.textContent++
-calculatePrice(e.target)
-
-
-    }else if (e.target.classList.contains('fa-minus')){
-        // e.target.previousElementSibling.textContent--
-    }else if (e.target.classList.contains('fa-trash-can')){
-        // e.target.previousElementSibling.textContent = '0'
-
+products.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-plus")) {
+    e.target.previousElementSibling.textContent++;
+    calculatePrice(e.target);
+  } else if (e.target.classList.contains("fa-minus")) {
+    if (e.target.nextElementSibling.textContent > 1) {
+      e.target.nextElementSibling.textContent--;
+      calculatePrice(e.target);
     }
+  } else if (e.target.classList.contains("fa-trash-can")) {
+    e.target.closest(".product").remove();
+    calculatePrice(e.target);
+  }
+});
 
-})
+const FREE_SHIPPING_LIMIT = 3000
+const SHIPPING_PRICE = 25.99
+const TAX_RATE = 0.18
+
+
 
 const calculatePrice = (btn) => {
   const discountedPrice = btn
     .closest(".product-info")
     .querySelector("#discounted-price");
 
-    const productPrice = btn
+  const productPrice = btn
     .closest(".buttons-div")
     .querySelector("#product-price");
 
-    const quantity = btn.parentNode.querySelector('#quantity')
-    productPrice.textContent = (quantity.textContent * discountedPrice.textContent).toFixed(2) 
+  const quantity = btn.parentNode.querySelector("#quantity");
+  productPrice.textContent = (
+    quantity.textContent * discountedPrice.textContent
+  ).toFixed(2);
+  calculateTotalPrice();
+};
+
+const calculateTotalPrice = () => {
+  const prices = document.querySelectorAll("#product-price");
+  const total = [...prices].reduce(
+    (sum, price) => sum + Number(price.textContent),
+    0
+  );
+
+const shippingPrice = total >= FREE_SHIPPING_LIMIT || total === 0 ? 0.00 : SHIPPING_PRICE
+
+
+const taxPrice = total * TAX_RATE
+const sum = total + taxPrice + shippingPrice
+
+  const selectedPrice = document.querySelector("#selected-price");
+  selectedPrice.textContent = total.toFixed(2);
+
+
+document.getElementById('shipping').textContent = shippingPrice.toFixed(2)
+document.getElementById('tax').textContent = taxPrice.toFixed(2)
+document.getElementById('total').textContent = sum.toFixed(2)
 
 };
+
+window.addEventListener('load', () => {
+calculateTotalPrice()
+})
+
+//const TAX_RATE = 0.18
